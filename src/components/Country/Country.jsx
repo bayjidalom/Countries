a2408ddd92158d0ee675549a653c927e3f155e08
bayjidalom <bayjidalom1005@gmail.com>
2026-03-16 21:14:57 +0600

    Conditional CSS

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+
+import { useState } from 'react';
 import './Country.css'
 
 const Country = ({ country }) => {
     // console.log(country.area.area)
 
-    const handler = () => {
-        console.log("Button Clicked.")
+    const [visited, setVisited] = useState(false)
+
+    const handlerVisited = () => {
+        // setVisited(true)          // visited theke change hobe na
+
+        // way -- basic
+        // if(visited){
+        //     setVisited(false)
+        // }else{
+        //     setVisited(true)
+        // }
+
+        // way--03 easy
+        // setVisited(visited ? false : true )
+
+        // way--04
+        setVisited(!visited)
+
+
     }
     return (
-        <div className='country'>
+        // <div className={`country border-lg text-center ${visited ? 'country-visited' : 'country-not-visited'}`}>
+
+        <div className={`country ${visited && 'country-visited'}`}>
+
             <img className='img' src={country.flags?.flags?.png} alt={country.flags.flags.alt} />
             <h3>Name : {country.name.common} </h3>
             <p>Population : {country.population.population}</p>
@@ -16,7 +37,9 @@ const Country = ({ country }) => {
             {/* <p>Capital : {country.capital.capital}</p> */}
             <p>Area : {country.area.area} - {country.area.area > 30000 ? "Big Country" : "Small country"}</p>
 
-            <button onClick={handler}> Not visited</button>
+            <button onClick={handlerVisited}> 
+                {visited ? "Visited" : "Not visited"}
+            </button>
 
         </div>
     );
